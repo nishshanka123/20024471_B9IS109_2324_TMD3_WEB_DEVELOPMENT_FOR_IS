6ded658f037b41411c57a9ab483b4b061b34a0ab
Nilusha Wimalasena <nw.nilusha@gmail.com>
2024-03-24 20:07:56 +0000

    Update json file

--- src/js/main.js
+++ src/js/main.js
@@ -54,25 +54,22 @@ var fitness_item_data = [
     },
     {
         "id" : "2",
-        "product_name" : "Indoor Exercise Bike",
-        "product_brand" : "Hit Fitness G6 PRO",
-        "product_price" : "360 €",
-        "produce_info" : "Exercise Bike is our next step in indoor exercise.",
-        "produce_desc" : "The perfect blend of cutting-edge features and affordability. Its sturdy frame allows a 140kg weight limit and has outstanding stability. It remains a compact design making it ideal for any living space, and storage. Like all our new Hit Fitness indoor exercise bikes it has a belt drive, allowing for a nearly silent ride in comparison to the old chain drive.\
-         With the 13 kg flywheel you get added comfort and fluidity to your workouts, allowing for smoother resistance shifting. \
-         The G6 Pro can also track your workout and fitness journey via the display console which provides you real-time data on your time, calories burned and speed. \
-         For extra peace of mind, this bike also comes with a 1 year home warranty. So you can challenge yourself and know your bike has you covered.",
+        "product_name" : "F200 Pro Power Rack",
+        "product_brand" : "HIT FITNESS",
+        "product_price" : "1165 €",
+        "produce_info" : "1 x Hit Fitness F200 Pro Power Rack Cable Pulley System",
+        "produce_desc" : "The F200 Pro Power Rack is the perfect commercial addition for gyms with heavy lifting clientele. It features a 60mm steel frame with 30 adjustable levels to provide a stable and secure foundation at an even heavier weight. A massive variety of resistance exercises can be performed using this power rack including; Shrugs, Squats, Chest Presses, Shoulder presses, Chin ups, Pull-ups, deadlifts, rack pulls, and lunges. Compatible with a range of attachments that will allow for a full range of workouts, and even more variety.",
         "features": {
-            "feature_1" : "Home Exercise Bike",
-            "feature_2" : "Display Console",
-            "feature 3" : "13kg Belt Driven Flywheel",
-            "feature_4" : "Adjustable Seat and Handle Bar Height",
-            "feature_5" : "Robust Sturdy Frame",
-            "feature_6" : "Measure: Speed / Distance / Time / Calories",
-            "feature_7" : "Transport Wheels for Easy Movement",
-            "feature_8" : "Water Bottle Holder",
-            "feature_9" : "Maximum User Weight: 140kg",
-            "feature_10" : "Warranty: 1 year home warranty",
+            "feature_1" : "1 x Hit Fitness F200 Pro Power Rack",
+            "feature_2" : "1 x Hit Fitness F200 Pro Power Rack Cable Pulley System",
+            "feature 3" : "Hit Fitness SB42 Deluxe Weight Bench",
+            "feature_4" : "Hit Fitness Home Olympic Weight Bar | 7ft (20kg)",
+            "feature_5" : "Hit Fitness Rubber Radial Olympic Weight Discs | 5kg",
+            "feature_6" : "Hit Fitness Rubber Radial Olympic Weight Discs | 10kg",
+            "feature_7" : "Hit Fitness Rubber Radial Olympic Weight Discs | 15kg",
+            "feature_8" : "Hit Fitness Rubber Radial Olympic Weight Discs | 20kg",
+            "feature_9" : "1 Pair of Hit Fitness Spring Collars 50mm",
+            "feature_10" : "1 Pair of Hit Fitness Spring Collars 50mm",
         },
         "spec" : {
             "bike_type" : "Studio",
@@ -107,18 +104,15 @@ var fitness_item_data = [
     },
     {
         "id" : "3",
-        "product_name" : "Indoor Exercise Bike",
-        "product_brand" : "Hit Fitness G6 PRO",
-        "product_price" : "360 €",
-        "produce_info" : "Exercise Bike is our next step in indoor exercise.",
-        "produce_desc" : "The perfect blend of cutting-edge features and affordability. Its sturdy frame allows a 140kg weight limit and has outstanding stability. It remains a compact design making it ideal for any living space, and storage. Like all our new Hit Fitness indoor exercise bikes it has a belt drive, allowing for a nearly silent ride in comparison to the old chain drive.\
-         With the 13 kg flywheel you get added comfort and fluidity to your workouts, allowing for smoother resistance shifting. \
-         The G6 Pro can also track your workout and fitness journey via the display console which provides you real-time data on your time, calories burned and speed. \
-         For extra peace of mind, this bike also comes with a 1 year home warranty. So you can challenge yourself and know your bike has you covered.",
+        "product_name" : "H5 Treadmill",
+        "product_brand" : "HIT FITNESS",
+        "product_price" : "899 €",
+        "produce_info" : "The Hit Fitness H5 Treadmill is the perfect treadmill for your home gym. Watch your workout stats add up on the easy-to-read 7 display and should you wish to follow a fitness program or watch your favourite shows, you can use your own tablet or smartphone (not included), resting it on the tablet holder* above the console. ",
+        "produce_desc" : "With 36 programs to choose from with varying speeds and inclines you are sure to find the perfect workout - suitable for all fitness levels. The manual option also lets you take control of the treadmill speed and incline - up to 18km/h and an incline range up to 15%.",
         "features": {
-            "feature_1" : "Home Exercise Bike",
-            "feature_2" : "Display Console",
-            "feature 3" : "13kg Belt Driven Flywheel",
+            "feature_1" : "Folding Treadmill ",
+            "feature_2" : "Incline Treadmill",
+            "feature 3" : "Colour Display",
             "feature_4" : "Adjustable Seat and Handle Bar Height",
             "feature_5" : "Robust Sturdy Frame",
             "feature_6" : "Measure: Speed / Distance / Time / Calories",
@@ -160,25 +154,22 @@ var fitness_item_data = [
     },
     {
         "id" : "4",
-        "product_name" : "Indoor Exercise Bike",
-        "product_brand" : "Hit Fitness G6 PRO",
-        "product_price" : "360 €",
-        "produce_info" : "Exercise Bike is our next step in indoor exercise.",
-        "produce_desc" : "The perfect blend of cutting-edge features and affordability. Its sturdy frame allows a 140kg weight limit and has outstanding stability. It remains a compact design making it ideal for any living space, and storage. Like all our new Hit Fitness indoor exercise bikes it has a belt drive, allowing for a nearly silent ride in comparison to the old chain drive.\
-         With the 13 kg flywheel you get added comfort and fluidity to your workouts, allowing for smoother resistance shifting. \
-         The G6 Pro can also track your workout and fitness journey via the display console which provides you real-time data on your time, calories burned and speed. \
-         For extra peace of mind, this bike also comes with a 1 year home warranty. So you can challenge yourself and know your bike has you covered.",
+        "product_name" : "Rowing Machine",
+        "product_brand" : "Hit Fitness",
+        "product_price" : "449 €",
+        "produce_info" : "The HIT Fitness Rowing Machine is a semi-commercial rower offering great value on the market and has all the features you need, including recording your distance, calories, total rows & time to help keep you in-check and in-line with your workouts.",
+        "produce_desc" : "The belt drive system on this seated row machine is powered by air & magnetic resistance and intensifies the faster you row. Fit for all heights and sizes with adjustable foot straps and a rail system stretching 113cm. After you have completed your full-body workout on this machine simply fold it up as seen to free up floor space or to tidy away until your next session.",
         "features": {
-            "feature_1" : "Home Exercise Bike",
-            "feature_2" : "Display Console",
-            "feature 3" : "13kg Belt Driven Flywheel",
-            "feature_4" : "Adjustable Seat and Handle Bar Height",
-            "feature_5" : "Robust Sturdy Frame",
-            "feature_6" : "Measure: Speed / Distance / Time / Calories",
-            "feature_7" : "Transport Wheels for Easy Movement",
-            "feature_8" : "Water Bottle Holder",
-            "feature_9" : "Maximum User Weight: 140kg",
-            "feature_10" : "Warranty: 1 year home warranty",
+            "feature_1" : "High quality, high functioning semi-commercial machine",
+            "feature_2" : "Smooth rail system to fit all sizes",
+            "feature 3" : "Full-body workout",
+            "feature_4" : "Foldable function for easy storage",
+            "feature_5" : "Ergonomic seat & handlebar",
+            "feature_6" : "Features a tablet holder for up to 6.5",
+            "feature_7" : "Belt driven",
+            "feature_8" : "Easy assembly",
+            "feature_9" : "Low maintenance machine",
+            "feature_10" : "Low impact workout",
         },
         "spec" : {
             "bike_type" : "Studio",
@@ -213,14 +204,11 @@ var fitness_item_data = [
     },
     {
         "id" : "5",
-        "product_name" : "Indoor Exercise Bike",
-        "product_brand" : "Hit Fitness G6 PRO",
-        "product_price" : "360 €",
-        "produce_info" : "Exercise Bike is our next step in indoor exercise.",
-        "produce_desc" : "The perfect blend of cutting-edge features and affordability. Its sturdy frame allows a 140kg weight limit and has outstanding stability. It remains a compact design making it ideal for any living space, and storage. Like all our new Hit Fitness indoor exercise bikes it has a belt drive, allowing for a nearly silent ride in comparison to the old chain drive.\
-         With the 13 kg flywheel you get added comfort and fluidity to your workouts, allowing for smoother resistance shifting. \
-         The G6 Pro can also track your workout and fitness journey via the display console which provides you real-time data on your time, calories burned and speed. \
-         For extra peace of mind, this bike also comes with a 1 year home warranty. So you can challenge yourself and know your bike has you covered.",
+        "product_name" : "50kg Core Weight Set",
+        "product_brand" : "HIT FITNESS",
+        "product_price" : "199 €",
+        "produce_info" : "This 7ft Olympic Barbell is a must have for anyone serious about their strength training. 30mm in diameter and is for use with 2 Olympic plates only.",
+        "produce_desc" : "Hit Fitness Rubber Radial Olympic Discs come equipped with tri-grip options. These radial Olympic weight plates have been designed specifically for performing a large variety of barbell exercises and exercises which do not require barbells, such as plate raises, which take advantage of the gripping options during a gym workout.",
         "features": {
             "feature_1" : "Home Exercise Bike",
             "feature_2" : "Display Console",
@@ -255,7 +243,7 @@ var fitness_item_data = [
         },
         "media": [
             {
-                "url": "./image/Hit-Fitness-H5-Treadmill-Main_800x@2x.webp",
+                "url": "./image/Rubber-Radial-Light-Mens-50Kg_3_600x_2x_700b2acd-6da2-4b11-8064-24f982eeb67c_800x@2x.webp",
                 "type": "image"
             },
             {
@@ -266,14 +254,11 @@ var fitness_item_data = [
     },
     {
         "id" : "6",
-        "product_name" : "Indoor Exercise Bike",
-        "product_brand" : "Hit Fitness G6 PRO",
-        "product_price" : "360 €",
-        "produce_info" : "Exercise Bike is our next step in indoor exercise.",
-        "produce_desc" : "The perfect blend of cutting-edge features and affordability. Its sturdy frame allows a 140kg weight limit and has outstanding stability. It remains a compact design making it ideal for any living space, and storage. Like all our new Hit Fitness indoor exercise bikes it has a belt drive, allowing for a nearly silent ride in comparison to the old chain drive.\
-         With the 13 kg flywheel you get added comfort and fluidity to your workouts, allowing for smoother resistance shifting. \
-         The G6 Pro can also track your workout and fitness journey via the display console which provides you real-time data on your time, calories burned and speed. \
-         For extra peace of mind, this bike also comes with a 1 year home warranty. So you can challenge yourself and know your bike has you covered.",
+        "product_name" : "Hex Dumbbells",
+        "product_brand" : "HIT FITNESS",
+        "product_price" : "177 €",
+        "produce_info" : "The Hit Fitness Rubber Hex Dumbbells Set - 5kg, 10kg and 15kg is a brilliant addition to gym floor based training because the hexagonal shape of the dumbbells provides additional stability.",
+        "produce_desc" : "These Hex Dumbbells can be used to perform a large variety of gym workouts which utilise free weight movements such as single arm rows, seated shoulder press, and bench press.Furthermore, using dumbbells as opposed to barbells can add an extra level of difficulty to the exercises as the weight must be controlled to a higher degree during dumbbell movements which activates the body's stabilizing musculature. This in turn can result in increased hypertrophy and strength gains in the aforementioned musculature as a result of the increased development and strengthening of stability muscles.",
         "features": {
             "feature_1" : "Home Exercise Bike",
             "feature_2" : "Display Console",
@@ -319,14 +304,11 @@ var fitness_item_data = [
     },
     {
         "id" : "7",
-        "product_name" : "Indoor Exercise Bike",
-        "product_brand" : "Hit Fitness G6 PRO",
-        "product_price" : "360 €",
-        "produce_info" : "Exercise Bike is our next step in indoor exercise.",
-        "produce_desc" : "The perfect blend of cutting-edge features and affordability. Its sturdy frame allows a 140kg weight limit and has outstanding stability. It remains a compact design making it ideal for any living space, and storage. Like all our new Hit Fitness indoor exercise bikes it has a belt drive, allowing for a nearly silent ride in comparison to the old chain drive.\
-         With the 13 kg flywheel you get added comfort and fluidity to your workouts, allowing for smoother resistance shifting. \
-         The G6 Pro can also track your workout and fitness journey via the display console which provides you real-time data on your time, calories burned and speed. \
-         For extra peace of mind, this bike also comes with a 1 year home warranty. So you can challenge yourself and know your bike has you covered.",
+        "product_name" : "Weightlifting Starter Set",
+        "product_brand" : "HIT FITNESS",
+        "product_price" : "450 €",
+        "produce_info" : "The HIT Fitness Adjustable Squat Rack is a perfect addition to any gym space as it allows athletes to perform all of the traditional compound movements without having to utilize all of the space that a power rack demands.",
+        "produce_desc" : "The Hit Fitness Adjustable Utility Weight Bench is equipped with an adjustable back support featuring 7 incline levels. This empowers beginners to start at a comfortable level and progressively challenge themselves as their strength and confidence grow.This bench offers a maximum weight capacity of 150kg, making it the perfect addition to this bundle.",
         "features": {
             "feature_1" : "Home Exercise Bike",
             "feature_2" : "Display Console",
@@ -372,7 +354,7 @@ var fitness_item_data = [
     }
     ];
 
-
+    //function loadProductList
     function loadProductList() {
         fitness_item_data.forEach((fitness_item)=>{
             var product_element = `<div class="product">
@@ -386,6 +368,7 @@ var fitness_item_data = [
     }
 
 
+    //function showDetail
     function showDetail() {
         var id = document.location.search.split("=")[1];
         var product_data;
